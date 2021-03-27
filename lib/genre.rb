2694b9f11bdc6f29f0e671c30452e30462403a72
rsmith <legendary.belief@gmail.com>
2021-03-26 20:33:59 -0700

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+class Genre
+    attr_accessor :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save 
+    end
+
+    def save 
+        @@all << self 
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def songs
+        Song.all {|song| song.genre == self}
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+end
 
